3404266bf916db33e63fc1d6c63d76b7dddd0b95
yoonhero <yoonhero06@naver.com>
2021-08-31 22:39:36 +0900

    #0.3

--- server.js
+++ server.js
@@ -4,16 +4,37 @@ import {
 } from "apollo-server-core";
 
 const typeDefs = gql`
+  type Movie {
+    title: String 
+    year: Int
+  }
 type Query {
-  hello : String 
+  movies: [Movie]
+  movie: Movie
+}
+type Mutation {
+  createMovie(title: String!):Boolean
+  deleteMovie(title: String!): Boolean
 }
 
 `
 
 const resolvers = {
   Query: {
-    hello: () => "world"
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title)
+      return true
+    },
+    deleteMovie: (_, args) => {
+      console.log(args)
+      return true
+    }
   }
+
 }
 
 
