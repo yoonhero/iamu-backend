2d77fd092c0441f29a0144a8ff72d5c19a9894de
yoonhero <yoonhero06@naver.com>
2021-08-31 22:23:50 +0900

    #0.1 apollo server

---
+++ server.js
@@ -0,0 +1,20 @@
+const { ApolloServer, gql } = require("apollo-server")
+
+const typeDefs = gql`
+  type Query {
+    hello : String 
+  }
+
+`
+
+const resolvers = {
+  Query: {
+    hello: () => "world"
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs, resolvers,
+})
+
+server.listen().then(() => console.log("Server is running on http://localhost:4000/"))
