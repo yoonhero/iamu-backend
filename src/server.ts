51e75d1f85e6f123e975e9c3405a0b97cd5f9e11
yoonhero <yoonhero06@naver.com>
2021-08-31 23:09:23 +0900

    #0.0 TS setup 1

---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+    id: Int
+  }
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, args) => {
+      console.log(args);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
